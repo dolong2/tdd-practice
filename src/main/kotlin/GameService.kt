61df24684873de372577ebd41e517d445f1e7bd7
dolong2 <k01066624566@gmail.com>
2025-02-16 16:40:39 +0900

    feat: checkAnswer 로직 작성

--- src/main/kotlin/GameService.kt
+++ src/main/kotlin/GameService.kt
@@ -3,6 +3,41 @@ class GameService(
 ) {
 
     fun checkAnswer(inputSet: Set<Int>): Boolean {
-        return false
+        val game = gameList.last()
+        val ballNumbers = game.ballNumbers
+
+        var ballCnt = 0
+        var strikeCnt = 0
+        inputSet.forEachIndexed { index, i ->
+            when {
+                ballNumbers.isStrike(i, index) -> {
+                    strikeCnt ++
+                    return@forEachIndexed
+                }
+                ballNumbers.isBall(i) -> {
+                    ballCnt++
+                    return@forEachIndexed
+                }
+            }
+        }
+
+        return if (strikeCnt == 3) {
+            println("정답입니다.")
+            true
+        }
+        else {
+            if (ballCnt == 0 && strikeCnt == 0)
+                println("낫싱")
+            else {
+                if (ballCnt != 0) {
+                    println("${ballCnt}볼 ")
+                }
+                if (strikeCnt != 0) {
+                    println("${strikeCnt}스트라이크")
+                }
+            }
+
+            false
+        }
     }
 }
